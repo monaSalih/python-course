044ebb9caad244d2edafd7544386e4d264152d72
Mona Salih <monaalobisat@gmail.com>
2026-04-23 01:25:18 +0300

    upload full material to review

---
+++ day3/Task3.py
@@ -0,0 +1,12 @@
+# ✅ Task 1: Scope Test
+# Create global variable
+# Access inside function
+# Try modifying it
+# ======================
+
+# ✅ Task 2:Create Module
+# Create file ut
+# ======================
+# ✅ Task 3:Division Program
+# Input 2 numbers
+# Handle division by zero
