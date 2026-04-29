5b7545009f162762df90eb29a38bfce7752aac34
Mona Salih <56797915+monaSalih@users.noreply.github.com>
2026-04-29 18:57:19 +0300

    Update session4.py

--- day4/session4.py
+++ day4/session4.py
@@ -1,4 +1,20 @@
-file = open("data.txt","r")
+# Read File
+file = open("data.txt", "r")
 content = file.read()
 print(content)
-#=================.
+file.close()
+
+# 📌 Read Line by Line
+with open("data.txt", "r") as file:
+    for line in file:
+        print(line.strip())
+# ======================
+
+with open("output.txt", "w") as file:
+    file.write("Hello World\n")
+    file.write("Python File I/O")
+
+# Append Data
+with open("output.txt", "a") as file:
+    file.write("\nNew Line Added")
+  
